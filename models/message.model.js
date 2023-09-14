32f4b6e29443c0822ea2f3245b890aa86741334a
gunjan <gunjan.soral@gmail.com>
2023-09-14 18:26:21 +0530

    socket is now working

--- models/message.model.js
+++ models/message.model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose')
 const messageSchema = new Schema({
   sender: { type: Schema.Types.ObjectId, ref: 'User' },
   content: String,
+  status: String,
   timestamp: { type: Date, default: Date.now },
 });
 
--- server.js
+++ server.js
@@ -1,39 +1,36 @@
 const express = require('express');
 const http = require('http');
-const mongoose = require('mongoose');
 const socketIo = require('socket.io');
-require('dotenv').config()
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const { socketControllers } = require('./controllers/socket.controller'); // Import socket controllers
 
 const app = express();
+
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+});
 
 app.use(express.json());
+app.use(cors()); // Add CORS middleware to allow cross-origin requests
 
 const { PORT, DB_URL } = process.env;
 
-// Connect to your MongoDB database
+// Connect to your database
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('DB connected'));
-// ... other Express app setup ...
+}).then(() => console.log('DB connected'))
+  .catch(error => console.error('DB connection error:', error)); // Handle database connection errors
 
-// Socket.io setup and logic go here
-
-io.on('connection', (socket) => {
-  console.log('User connected');
-
-  socket.on('chat message', (message) => {
-    // Broadcast the message to all connected clients
-    io.emit('chat message', message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
+//socket controllers
+socketControllers(io);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
